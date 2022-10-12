fb8ec649e87a52df3e0582cb9fa58dabf4a919e4
William Schroeder <w.schroeder55609@gmail.com>
2022-10-12 18:58:16 -0500

    checkpoint

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
+a = ['255', '255', '255']
+b = "apple"
+'''# index and list
+for index, value in enumerate(a):
+    if value == "255":
+        a[index] = "not a bannana"
+print(a)'''
+import tkinter
+import PIL
 from PIL import Image, ImageTk
-import tkinter as tk
 
-debounce = False
-
-
-def load(img1, img2):
-    firstimg = (
-
-    )
-
-
+def check(least_list,greater_list):
+    for place, rgb in enumerate(greater_list):
+        if place > (len(least_list) -1):
+            greater_list[place] = (255, 0 ,0,255)
+    for place, rgb in enumerate(least_list):
+        if not least_list[place] == greater_list[place]:
+            least_list[place] = (255, 0, 0, 255)
+            greater_list[place] = (255, 0, 0,255)
+    return [least_list,greater_list]
 def get_image(image_path):
-    image = Image.open(image_path).convert("L")
-    pixel_values = list(image.getdata())
+    image = Image.open(image_path)
+    size = image.size
+    image_rgb = image.convert("RGB")
+    pixel_values = list(image_rgb.getdata())
+    return [pixel_values, size[0], size[1]]
 
-    return pixel_values
 
+c = get_image('test1.png')
+c2 = get_image('test2.png')
+if len(c[0]) >= len(c2[0]):
+    checking = check(c2[0], c[0])
+    cr = checking[1]
+    c2r = checking[0]
+else:
+    checking = check(c[0], c2[0])
+    cr = checking[0]
+    c2r = checking[1]
+crim = PIL.Image.new(mode="RGB", size=(c[1], c[2]))
+crim.save("img1.png")
+crims = Image.open("img1.png")
+crimse = get_image("img1.png")
 
-while True:
-    if not debounce:
-        root = tk.Tk()
-        root.geometry('600x500+250+200')
-        frame = tk.Frame()
-        frame.pack()
-        firstimgtext = tk.Text(root,
-                               height=1,
-                               width=25
-                               )
-        firstimgtext.pack()
-        firstimgtext.place(x=-0, y=0)
-        secondimgtext = tk.Text(root,
-                                height=1,
-                                width=25
-                                )
-        secondimgtext.pack()
-        secondimgtext.place(x=390, y=0)
-        load_button = tk.Button(text="load",
-                                command=lambda: load(str(firstimgtext.get("1.0", 'end-1c')),
-                                                     str(secondimgtext.get("1.0", 'end-1c'))))
-        root.mainloop()
-    else:
-        print(get_image(image_path='./test.png'))
+x = 0
+y = 0
+pix = crims.load()
+for rgb in cr:
+    x = x + 1
+    if x > c[1] - 1:
+        x = x - c[1]
+        y = y + 1
+    if y > c[2] - 1:
+        y = y - c[2]
+    pix[x, y] = rgb
+crims.save("img1.png")
---
+++ readme.md
@@ -0,0 +1,20 @@
+<h1 align="center">Image Comparer</h1>
+<h2>What is Image Comparer?</h2>
+<p>Image Comparer is a code that will take two images that you give it, get all the pixels, then get the rgb color for 
+every pixel. After it has done that it will compare every pixel. If it finds that the pixel is not the same it will
+color that one pixel completely red it will then save the new images to files
+<br><br>
+first image
+<br>
+<img src="test1.png">
+<br>
+second image
+<br>
+<img src="test2.png">
+<br>
+results
+<br>
+<img src="img1.png">
+<br>
+the pixels surrounding the letters have turned red because they are different in the two images
+</p>
